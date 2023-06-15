47ccc331a9165ee919fdcfecdb731da1dcec2581
cko003 <cko003@bucknell.edu>
2023-06-15 16:20:29 -0400

    added footer and typing animation

--- script.js
+++ script.js
@@ -5,13 +5,38 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky");
        }else{
             $('.navbar').removeClass("sticky");
-       } 
+       }
+       if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+       }else{
+            $('.scroll-up-btn').removeClass("show");
+       }
+    });
+
+    // Slide Up Script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
 
     // Toggle Menu / NavBar Script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
+    });
+
+    // Typing Animation Script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Researcher"],
+        typeSpeed: 100, 
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Researcher"],
+        typeSpeed: 100, 
+        backSpeed: 60,
+        loop: true
     });
 
     // Carousel Script
