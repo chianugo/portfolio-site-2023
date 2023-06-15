e361f9ae59688c4ce860bc573915faa6dada6e0a
cko003 <cko003@bucknell.edu>
2023-06-15 13:29:40 -0400

    projects section complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // Navigation Bar Script
     $(window).scroll(function(){
        if(this.scrollY > 40){
             $('.navbar').addClass("sticky");
@@ -11,5 +12,27 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
+    });
+
+    // Carousel Script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHover: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false,
+            },
+            600:{
+                items: 2 ,
+                nav: false,
+            },
+            1000:{
+                items: 3,
+                nav: false,
+            }
+        }
     });
 });
